78e37c634182fe3ab528fb12d955b77eb1bc9846
Codeinx <aseyeged@gmail.com>
2022-02-01 16:02:42 +0100

    my first commit

---
+++ src/test/java/com/codeinx/APIproject/ApIprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeinx.APIproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
